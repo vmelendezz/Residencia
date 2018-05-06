b69370e6a14ee4dce2e099d9eba58997d9a1b775
Raul67 <zepara_raul67@hotmail.com>
2018-05-06 00:23:15 -0500

    index modificado

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 //1.- se cargan los archivos
- require "./controller/rutes.php";
- require "./controller/templates.php";
+ require "./private/controller/rutes.php";
+ require "./private/controller/templates.php";
 
 //2.- se crea un objeto de la clase Rutes el cual contiene las variables globales y los metodos mandados a llamar en el constructor
  $rutes = new Rutes ();
